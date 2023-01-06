58c0423f44e37ddcedddf323bc0f21956df49826
Ned Regina <nregina@hbs.edu>
2023-01-06 13:15:53 -0500

    Initial commit

---
+++ src/main/java/com/nedr/sonarcloudtest/SonarcloudTestApplication.java
@@ -0,0 +1,13 @@
+package com.nedr.sonarcloudtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudTestApplication.class, args);
+	}
+
+}
